c5d681b79d428cea3bb693ce9d72e93380ac4bb3
Matthew Caldwel <caldwellysr@gmail.com>
2017-01-25 17:48:53 -0500

    finished part 2

--- src/todos.php
+++ src/todos.php
@@ -1,7 +1,34 @@
 <?php
-// output a sample todo list
-echo "Todos:\n";
 
-echo "- Task 1\n";
-echo '- Task 2' . "\n";
-echo '- Task 3' . PHP_EOL;
+    $todos = array(
+        "Cook dinner",
+        "Feed the dog",
+        "Gas the car",
+        "Play video games"
+    );
+
+    /*
+    foreach ($todos as $key => $todo) {
+        echo $key . ": " . $todo . PHP_EOL;
+    }
+    */
+
+    for ($index = 0; $index < count($todos); $index++) {
+        if ($index == 0 || $index == 2) {
+            echo "Completed -- " . $todos[$index] . PHP_EOL;
+        } else {
+            echo $todos[$index] . PHP_EOL;
+        }
+    }
+
+    /*
+    $index = 0;
+
+    while ($index < count($todos)) {
+        echo $todos[$index] . PHP_EOL;
+        $index++;
+    }
+    */
+    
+
+?>
